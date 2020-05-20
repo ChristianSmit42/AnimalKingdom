1fd1fa156b796fdb3900d5c931455190d711f479
Christian Smit <chrsmit@web.de>
2020-05-20 11:06:22 +0200

    ADD Animal and Reptile variables and methods

--- src/main/java/de/neuefische/animalkingdom/model/Animal.java
+++ src/main/java/de/neuefische/animalkingdom/model/Animal.java
@@ -2,4 +2,16 @@ package de.neuefische.animalkingdom.model;
 
 public class Animal {
     private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/de/neuefische/animalkingdom/model/Reptile.java
+++ src/main/java/de/neuefische/animalkingdom/model/Reptile.java
@@ -1,4 +1,31 @@
 package de.neuefische.animalkingdom.model;
 
 public class Reptile extends Animal{
+    int heartchambers = 4;
+    boolean carnivore;
+    double temperature=15; // As starting value
+
+    public Reptile(String name, boolean carnivore) {
+        super(name);
+        this.carnivore = carnivore;
+    }
+
+    public int getHeartchambers() {
+        return heartchambers;
+    }
+
+    public boolean isCarnivore() {
+        return carnivore;
+    }
+
+    public void setCarnivore(boolean carnivore) {
+        this.carnivore = carnivore;
+    }
+
+    public String timeToReachBodytemperature(double actualTemperature, double desiredTemperature){
+        if(desiredTemperature<=actualTemperature){
+            return "get out of the Sun now!";
+        }
+        return "Time to desired Temperature: " + (desiredTemperature-actualTemperature)*0.3 + " Minutes";
+    }
 }
